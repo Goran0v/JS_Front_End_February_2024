b61ba17cf44284796eb1122086e6215c258bfb37
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-20 17:36:01 +0200

    Add the exercises

---
+++ HTTP And AJAX Exercise/HTTP & AJAX Exercise/03.Phonebook/app.js
@@ -0,0 +1,67 @@
+function attachEvents() {
+    const baseUrl = 'http://localhost:3030/jsonstore/phonebook';
+
+    const loadButtonElement = document.getElementById('btnLoad');
+    const createButtonElement = document.getElementById('btnCreate');
+    const phoneBookElement = document.getElementById('phoneBook');
+    const personElement = document.getElementById('person');
+    const phoneElement = document.getElementById('phone');
+
+    loadButtonElement.addEventListener('click', () => {
+    phoneBookElement.innerHTML = '';
+
+        fetch(baseUrl)
+            .then(res => res.json())
+            .then(data => {
+                Object.values(data)
+                    .forEach(entry => {
+                        phoneBookElement.appendChild(createEntryElement(entry));
+                    });
+            });
+    });
+
+    createButtonElement.addEventListener('click', () => {
+        fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                person: personElement.value,
+                phone: phoneElement.value,
+            })
+        })
+            .then(res => res.json())
+            .then(entry => {
+                const liElement = createEntryElement(entry);
+        
+                phoneBookElement.appendChild(liElement);
+                personElement.value = '';
+                phoneElement.value = '';
+            });
+        
+    });
+
+    function createEntryElement(entry) {
+        const liElement = document.createElement('li');
+        liElement.textContent = `${entry.person}: ${entry.phone}`;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+
+        deleteButton.addEventListener('click', () => {
+            fetch(`${baseUrl}/${entry._id}`, {
+                method: 'DELETE'
+            })
+                .then(() => {
+                    liElement.remove();
+                });
+        });
+
+        liElement.appendChild(deleteButton);
+
+        return liElement;
+    }
+}
+
+attachEvents();
